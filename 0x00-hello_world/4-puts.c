f825291070ad660b734edbb17929662ebdb8bca2
smartapaul <smarterpaul@gmail.com>
2023-02-19 11:33:07 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+char mystr[] = "Programming is like building a multilingual puzzle\n"; /*initializing string*/
+puts (mystr); /*writing string to stdout*/
+
+return (0);
+
+}
