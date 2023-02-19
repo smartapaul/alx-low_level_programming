97b2307d9a9795a9d92c358ed232ad71a2b1546b
smartapaul <smarterpaul@gmail.com>
2023-02-19 14:41:34 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,16 @@
 /**
- * File: 5-printf.c
- * Auth: Paul Wahome <smarterpaul@gmail.com>
- */
+* File: five-printf.c
+* Auth: Paul Wahome <smarterpaul@gmail.com>
+*/
 #include <stdio.h>
 /**
- * main - starinnt of the program
- * Description: prints "with proper grammar, but the outcome is a piece of art,
- *
- *  Return: zero
- *  ruturn - gives zero
- *  Description: Always return if true
- */
+* main - starinnt of the program
+* Description: prints "with proper grammar, but the outcome is a piece of art,
+*
+* Return:0 for success
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	 return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
