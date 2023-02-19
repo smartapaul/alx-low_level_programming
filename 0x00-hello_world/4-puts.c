a0d84e5d88323a0e79fd2457c14c6941c24b2738
smartapaul <smarterpaul@gmail.com>
2023-02-19 13:51:45 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,27 +2,16 @@
  * File: 4-puts.c
  * Auth: Paul Wahome <smarterpaul@gmail.com>
  */
-
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * calling funtion main
- *Description: main fuction is the entry point for our program
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
-int main(void)
 
+int main(void)
 {
-/**puts - initializing string to call with puts function
-*
-*Description: initializes mystr fuction.
-*/
-        char mystr[] = "\"Programming is like building a multilingual puzzle\n";
-/**writing string to stdout
-* put calls mystr function value
-*/
-        puts(mystr);
-/**
-*Always return zero
-*/
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
