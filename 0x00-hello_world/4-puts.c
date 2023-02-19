5516acbf59b634ed8812443c244c2a2aa636e734
smartapaul <smarterpaul@gmail.com>
2023-02-19 12:11:05 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int main()
+/*calling funtion main*/
+int main(void)
 {
-	/**
-	 * initializing string
-	 * to call with puts function
-	 */
-	char mystr[] = "Programming is like building a multilingual puzzle\n"; 
-	/*writing string to stdout*/
-	puts (mystr);
+/**
+ * initializing string
+ * to call with puts function
+ */
+	char mystr[]= "Programming is like building a multilingual puzzle\n"; 
+/*writing string to stdout*/
+	puts(mystr);
 
 
 	return (0);
