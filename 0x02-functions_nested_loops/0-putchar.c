e4d7802cfcdeb1d8b3138ed8cc251f105d5a8eb7
smartapaul <smarterpaul@gmail.com>
2023-02-23 22:23:25 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* main - main function
-* _putchar: prints the alphabet putchar
-* Return: always 0
-*/
+ * main - print _putchar
+ * is written by paul
+ * Return: 0
+ */
+
 int main(void)
 {
 _putchar('_');
@@ -16,5 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-retun(0);
+return (0);
 }
+
